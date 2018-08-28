ad2e4c4aa0ee1e194a0e0ce303bdcbcb234afb45
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-28 02:13:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
-  const list = document.querySelectorAll('grand-node')
+  const list = document.querySelectorAll('#grand-node')
   return list
 }
