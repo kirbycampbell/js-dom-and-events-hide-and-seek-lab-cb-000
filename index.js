2dab2831366230e3ac9f4f86192b868f1bbdee92
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-28 01:52:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function getFirstSelector(selector){
 function nestedTarget(){
   return document.getElementById('nested');
 }
+
+function increaseRankBy(n){
+  const list = document.querySelectorAll('ranked-list')
+  for (let i = 0; i < list.length; i++) {
+    let num = parseInt(list[i]) + 1;
+    return num
+  }
+}
