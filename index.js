cf1045b527213092e0e13a5fcb6f6f5e7e7d97f4
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-28 01:54:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget(){
 function increaseRankBy(n){
   const list = document.querySelectorAll('ranked-list')
   for (let i = 0; i < list.length; i++) {
-    list[i] = parseInt(list[i]) + 1;
+    parseInt(list[i]) = parseInt(list[i]) + 1;
     //return num
   }
 }
